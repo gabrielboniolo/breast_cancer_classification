061d45945574a25e422bdc5e65c6ebc278fa068c
gabrielboniolo <gabrielboniolo@gmail.com>
2026-04-12 14:08:30 -0300

    feat: CORS

--- app/main.py
+++ app/main.py
@@ -1,10 +1,20 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from database import Base, engine
 from routers import init_routers 
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 Base.metadata.create_all(bind=engine)
 
